d51b1b753ace0444fedd87f99239f2f96c240d9c
jj <giovanni.giorgi@nttdata.com>
2014-05-13 18:24:32 +0200

    First micro service version

--- micro-service1/src/main/java/com/gioorgi/microservice/HelloWorld.java
+++ micro-service1/src/main/java/com/gioorgi/microservice/HelloWorld.java
@@ -3,11 +3,18 @@ package com.gioorgi.microservice;
 
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ *  Happily run with
+ * java -jar build/libs/micro-service1.jar  --debug  --spring.config.location=src/main/java/config/
+ * @author jj
+ *
+ */
 @Controller
-@EnableAutoConfiguration
+@EnableAutoConfiguration()
 public class HelloWorld {
 
 	@RequestMapping("/")
@@ -17,6 +24,7 @@ public class HelloWorld {
     }
 
     public static void main(String[] args) throws Exception {
+    	   	
         SpringApplication.run(HelloWorld.class, args);
     }
 }
