666d2f58cd243ea10f3c78757e509ed363bfe3e0
jj <giovanni.giorgi@nttdata.com>
2014-05-14 10:32:35 +0200

    Added support for spring loaded and boot command

--- micro-service1/src/main/java/com/gioorgi/microservice/HelloWorld.java
+++ micro-service1/src/main/java/com/gioorgi/microservice/HelloWorld.java
@@ -1,6 +1,7 @@
 package com.gioorgi.microservice;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -9,7 +10,11 @@ import org.springframework.web.bind.annotation.*;
 
 /**
  *  Happily run with
- * java -jar build/libs/micro-service1.jar  --debug  --spring.config.location=src/main/java/config/
+ * java -jar build/libs/micro-service1.jar
+ * Look at
+ *  http://localhost:8080/configprops
+ * for configured properties 
+ * # --debug  --spring.config.location=src/main/java/config/
  * @author jj
  *
  */
@@ -17,13 +22,29 @@ import org.springframework.web.bind.annotation.*;
 @EnableAutoConfiguration()
 public class HelloWorld {
 
+	@Value("${app.version:UNKNOWN}")
+	String version;
+	
 	@RequestMapping("/")
     @ResponseBody
     String home() {
         return "Hello World!";
     }
 
-    public static void main(String[] args) throws Exception {
+	@RequestMapping("/about")
+    @ResponseBody
+    String about() {
+        return "The Gradle+SpringBoot+SpringLoaded 2014 JJ's Demo is here. Demo Version:"+version;
+    }
+
+	
+	
+	
+    public void setVersion(String version) {
+		this.version = version;
+	}
+
+	public static void main(String[] args) throws Exception {
     	   	
         SpringApplication.run(HelloWorld.class, args);
     }
